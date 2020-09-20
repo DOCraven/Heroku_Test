a3804b89d0d6ec95a12677445e49d5a1189bf316
DOCraven <brg.tman@gmail.com>
2020-09-20 23:07:36 +1000

    t

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import dash
+import base64
+import datetime
+import io
 
-import dash_html_components as html
+import dash
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
-from dash.dependencies import Output, Input, State
-from dash.exceptions import PreventUpdate
+import dash_html_components as html
+import dash_table
+
+import pandas as pd
 
 # This stylesheet makes the buttons and table pretty.
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -13,76 +18,79 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_ca
 server = app.server
 
 app.layout = html.Div([
-    # The memory store reverts to the default on every page refresh
-    dcc.Store(id='memory'),
-    # The local store will take the initial data
-    # only the first time the page is loaded
-    # and keep it until it is cleared.
-    dcc.Store(id='local', storage_type='local'),
-    # Same as the local store but will lose the data
-    # when the browser/tab closes.
-    dcc.Store(id='session', storage_type='session'),
-    html.Table([
-        html.Thead([
-            html.Tr(html.Th('Click to store in:', colSpan="3")),
-            html.Tr([
-                html.Th(html.Button('memory', id='memory-button')),
-                html.Th(html.Button('localStorage', id='local-button')),
-                html.Th(html.Button('sessionStorage', id='session-button'))
-            ]),
-            html.Tr([
-                html.Th('Memory clicks'),
-                html.Th('Local clicks'),
-                html.Th('Session clicks')
-            ])
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
         ]),
-        html.Tbody([
-            html.Tr([
-                html.Td(0, id='memory-clicks'),
-                html.Td(0, id='local-clicks'),
-                html.Td(0, id='session-clicks')
-            ])
-        ])
-    ])
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
 ])
 
-# Create two callback for every store.
-for store in ('memory', 'local', 'session'):
 
-    # add a click to the appropriate store.
-    @app.callback(Output(store, 'data'),
-                  [Input('{}-button'.format(store), 'n_clicks')],
-                  [State(store, 'data')])
-    def on_click(n_clicks, data):
-        if n_clicks is None:
-            # prevent the None callbacks is important with the store component.
-            # you don't want to update the store for nothing.
-            raise PreventUpdate
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
 
-        # Give a default data dict with 0 clicks if there's no data.
-        data = data or {'clicks': 0}
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
 
-        data['clicks'] = data['clicks'] + 1
-        return data
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
 
-    # output the stored clicks in the table cell.
-    @app.callback(Output('{}-clicks'.format(store), 'children'),
-                  # Since we use the data prop in an output,
-                  # we cannot get the initial data on load with the data prop.
-                  # To counter this, you can use the modified_timestamp
-                  # as Input and the data as State.
-                  # This limitation is due to the initial None callbacks
-                  # https://github.com/plotly/dash-renderer/pull/81
-                  [Input(store, 'modified_timestamp')],
-                  [State(store, 'data')])
-    def on_data(ts, data):
-        if ts is None:
-            raise PreventUpdate
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
 
-        data = data or {}
+        html.Hr(),  # horizontal line
 
-        return data.get('clicks', 0)
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8077, threaded=True)
+    app.run_server(debug=True)
